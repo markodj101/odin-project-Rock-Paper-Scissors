610c0f6123ba5522c73ca7af09ac1e2a8e565544
markodj101 <markodj10@gmail.com>
2023-12-26 00:18:17 +0100

    Made my js file with two functions: Computer Choice and Round

---
+++ external.js
@@ -0,0 +1,17 @@
+function getComputerChoice(){
+    let lista = ["list", "kamen", "makaze"]
+    let izborr = Math.floor(Math.random() * lista.length)
+    return lista[izborr]
+}
+
+
+
+function playRound(playerSelection, computerSelection) {
+    let player = String(playerSelection.toLowerCase())
+    
+  }
+   
+  const playerSelection = prompt("Unesi list ili kamen ili makaze:")
+  const computerSelection = getComputerChoice();
+  console.log(getComputerChoice());
+  console.log(playRound(playerSelection, computerSelection));
