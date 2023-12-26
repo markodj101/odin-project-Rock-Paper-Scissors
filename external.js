ecf50e4e72e74078defbb7c55846b5c675f656c4
markodj101 <markodj10@gmail.com>
2023-12-26 20:19:21 +0100

    Zavrsio game funkciju i porpavio kada je igrac i racunar isto odigraju

--- external.js
+++ external.js
@@ -1,5 +1,33 @@
 function game(){
     
+    let igrac = 0, kompjuter = 0
+    for (let i = 0; i < 5; i++) {
+         playerSelection = prompt("Unesi list ili kamen ili makaze:")
+        let computerSelection = getComputerChoice();
+        let rezultat = playRound(playerSelection, computerSelection);
+        if (rezultat.includes("Win")){
+            igrac +=1;
+        }
+        else if (rezultat.includes("Tie")){
+            igrac +=1;
+            kompjuter +=1;
+        }
+        else  {
+            kompjuter +=1;
+        }
+        console.log(rezultat)
+        console.log ("Ovo je pratija broj :" + (i+1))
+        
+    }
+    if (igrac>kompjuter){
+        console.log("Igrac je pobjedio")
+    }
+    else if (kompjuter>igrac){
+        console.log("Kompjuter je pobjedio")
+    }
+    else{
+        console.log("Nema pobjednika")
+    }
 }
 
 
@@ -14,11 +42,47 @@ function getComputerChoice(){
 
 
 function playRound(playerSelection, computerSelection) {
+    console.log(getComputerChoice());
     let player = String(playerSelection.toLowerCase())
-    
+    if (player == "makaze"){
+        if (computerSelection == "kamen"){
+            return "You Lose! Rock beats Scissors"
+        }
+        else if (computerSelection =="makaze"){
+            return "Tie!"
+        }
+        else {
+            return "You Win! Scissors beats Paper"
+        }
+    }
+    else if (player == "list"){
+        if (computerSelection == "kamen"){
+            return "You Win! Paper beats Rock"
+        }
+        else if (computerSelection =="makaze"){
+            return "You Lose! Scissors beats Paper"
+        }
+        else {
+            return "Tie!"
+        }
+    }
+    else if (player == "kamen"){
+        if (computerSelection == "kamen"){
+            return "Tie!"
+        }
+        else if (computerSelection =="makaze"){
+            return "You Win! Rock beats Scissors"
+        }
+        else {
+            return "You Lose! Paper beats Scissors"
+        }
+    }
+    else {
+
+        return  playerSelection = prompt("Igrac nije dobro unio unesite opet")
+    }
   }
-   
-  const playerSelection = prompt("Unesi list ili kamen ili makaze:")
-  const computerSelection = getComputerChoice();
-  console.log(getComputerChoice());
-  console.log(playRound(playerSelection, computerSelection));
+
+  let playerSelection;
+
+  game()
